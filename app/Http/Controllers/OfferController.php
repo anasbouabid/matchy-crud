70c9005397bb2a300669b5c17bf037a12bdc2599
Anas BOUABID <abouabid@leyton.com>
2023-07-23 04:40:06 +0100

    offer api

--- app/Http/Controllers/OfferController.php
+++ app/Http/Controllers/OfferController.php
@@ -7,59 +7,79 @@ use Illuminate\Http\Request;
 
 class OfferController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
+    // Get all offers
     public function index()
     {
-        //
+        $offers = Offer::all();
+        return response()->json($offers);
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
+    // Get a specific offer by ID
+    public function show($id)
     {
-        //
+        $offer = Offer::find($id);
+
+        if (!$offer) {
+            return response()->json(['message' => 'Offer not found'], 404);
+        }
+
+        return response()->json($offer);
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
+    // Create a new offer
     public function store(Request $request)
     {
-        //
+        $offer = new Offer();
+        $offer->company = $request->input('company');
+        $offer->title = $request->input('title');
+        $offer->type = $request->input('type');
+        $offer->attendance = $request->input('attendance');
+        $offer->period = $request->input('period');
+        $offer->gratifications = $request->input('gratifications');
+        $offer->tags = $request->input('tags');
+        $offer->description = $request->input('description');
+        $offer->created_by = $request->input('created_by'); // Assuming you pass the user ID in the request
+
+        $offer->save();
+
+        return response()->json($offer, 201);
     }
 
-    /**
-     * Display the specified resource.
-     */
-    public function show(offer $offer)
+    // Update an existing offer
+    public function update(Request $request, $id)
     {
-        //
+        $offer = Offer::find($id);
+
+        if (!$offer) {
+            return response()->json(['message' => 'Offer not found'], 404);
+        }
+
+        // Update offer data
+        $offer->company = $request->input('company');
+        $offer->title = $request->input('title');
+        $offer->type = $request->input('type');
+        $offer->attendance = $request->input('attendance');
+        $offer->period = $request->input('period');
+        $offer->gratifications = $request->input('gratifications');
+        $offer->tags = $request->input('tags');
+        $offer->description = $request->input('description');
+        
+        $offer->save();
+
+        return response()->json($offer);
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(offer $offer)
+    // Delete an offer
+    public function destroy($id)
     {
-        //
-    }
+        $offer = Offer::find($id);
 
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, offer $offer)
-    {
-        //
-    }
+        if (!$offer) {
+            return response()->json(['message' => 'Offer not found'], 404);
+        }
 
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(offer $offer)
-    {
-        //
+        $offer->delete();
+
+        return response()->json(['message' => 'Offer deleted'], 200);
     }
 }
